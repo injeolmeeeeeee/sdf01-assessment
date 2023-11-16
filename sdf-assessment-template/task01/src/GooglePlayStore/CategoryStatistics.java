dc3058c9647d2d5964eab211c7b36da8f500c7bc
injeolmeeeeeee <86404260+injeolmeeeeeee@users.noreply.github.com>
2023-11-16 12:38:49 +0800

    task01

---
+++ sdf-assessment-template/task01/src/GooglePlayStore/CategoryStatistics.java
@@ -0,0 +1,41 @@
+package GooglePlayStore;
+
+public class CategoryStatistics {
+
+    private static int count;
+    private static int discardedCount;
+    private static String categoryName;
+    private static double totalRating;
+    private static double highestAppRating;
+    private static double lowestAppRating;
+    private static String highestAppName;
+    private static String lowestAppName;
+
+    //get lowest/highest ratings, by comparing each app
+    public void appStats(String appName, double rating) {
+        count++;
+
+        if(rating< lowestAppRating) {
+            lowestAppRating = rating;
+            lowestAppName = appName;
+        } else if (rating > highestAppRating) {
+            highestAppRating = rating;
+            highestAppName = appName;
+        }
+        totalRating += rating; 
+    }  
+
+    public static void discardedCount() {
+        discardedCount++;
+    }
+
+    public static void printStats() {
+        System.out.println("Category: " + categoryName);
+        System.out.println("Highest: " + highestAppName + ", <" + highestAppRating + ">");
+        System.out.println("Lowest: " + lowestAppName + ", <" + lowestAppRating + ">");
+        System.out.println("Average: "  + (totalRating/count));
+        System.out.println("Count: " + count);
+        System.out.println("Discarded: " + discardedCount);
+
+    }
+}
--- sdf-assessment-template/task01/src/GooglePlayStore/Main.java
+++ sdf-assessment-template/task01/src/GooglePlayStore/Main.java
@@ -1,9 +1,69 @@
 package GooglePlayStore;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main{
 
     public static void main(String[] args) {
-        
-    }
 
+        if (args.length <= 0) {
+            System.out.println("You have entered an invalid file name");
+            System.exit(1);
+        }
+
+        Map<String, CategoryStatistics> categoryStatsMap = new HashMap<>();
+        
+        System.out.printf("Processing file: /s%n", args[0]);
+
+        try {FileReader fr = new FileReader(args[0]);
+        BufferedReader br = new BufferedReader(fr);
+            //read header first
+        String header = br.readLine();
+
+        String line;
+        int totalLines = 0;
+
+        while ((line = br.readLine()) != null) {
+            totalLines++;
+
+            String[] fields = line.split(",");
+
+            if (fields.length<3) {
+                continue;
+            }
+
+            String appName = fields[0].trim().toUpperCase();
+            String categoryName = fields[1].trim().toUpperCase();
+            double rating;
+
+            try {
+                rating = Double.parseDouble(fields[2]);
+            } catch (NumberFormatException e) {
+                CategoryStatistics.discardedCount();;
+                continue;
+            }
+
+            CategoryStatistics categoryStatistics = categoryStatsMap.get(categoryName);
+            if (categoryStatistics == null) {
+                categoryStatistics = new CategoryStatistics();
+                categoryStatsMap.put(categoryName, categoryStatistics);
+            }
+            categoryStatistics.appStats(appName, rating);
+        }
+
+        categoryStatsMap.forEach((category, stats) -> {
+            System.out.println("Catergory: " + category);
+            CategoryStatistics.printStats();
+        });
+
+        System.out.println("Total lines in file: " + totalLines);
+        br.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }  
+    }
 }
