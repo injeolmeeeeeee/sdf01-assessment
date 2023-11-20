a5186714753dda8a2cc13f5809f722e305b5e483
injeolmeeeeeee <86404260+injeolmeeeeeee@users.noreply.github.com>
2023-11-20 20:56:41 +0800

    practice push

--- sdf-assessment-template/task02/src/ShoppingBudget/Client.java
+++ sdf-assessment-template/task02/src/ShoppingBudget/Client.java
@@ -8,110 +8,182 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
-public class Client implements Runnable{
-
-    private static Socket clientSocket;
-
-    public Client(Socket clientSocket) {
-        this.clientSocket = clientSocket;
-    }
-
-    Map<String, Product> receivedInfo = new HashMap<>();
+public class Main {
     
-    @Override
-    public void run() {
+    static String server;
+    static int port;
+    static Product product = new Product();
+    static List<Product> products = new ArrayList<>();
+    
 
-        try (InputStream is = clientSocket.getInputStream();
-            OutputStream os = clientSocket.getOutputStream();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            OutputStreamWriter osw = new OutputStreamWriter(clientSocket.getOutputStream());
-            BufferedWriter bw = new BufferedWriter(osw);
-            BufferedReader br = new BufferedReader(isr)){
-
-                int itemCount = 0;
-                int serverBudget = 0;
-                
-                String line = br.readLine();
-
-                while ((line != null) && (!line.isEmpty())) { //skipping through empty and error lines
-                    if (line.startsWith("item_count")) {
-                        line = line.substring(13);
-                        itemCount = Integer.parseInt(line);
-                    }
-                    for (int i = 0; i < itemCount; i++) {//need to read message and save into list n times, where n = item_count, then the comparison can be done
-                        line = br.readLine();
-                        String[] information = line.split("\n"); //store message in Array
-                        
-                        if (line.startsWith("request_id: ")) {
-                            String requestID = information[0].substring(12);
-                        } else if (line.startsWith("prod_id: ")) {
-                            int productID = Integer.parseInt(information[5].substring(10));
-                        } else if (line.startsWith("title: ")) {
-                            String title = information[6].substring(8);
-                        } else if (line.startsWith("price: ")) {
-                            double price = Double.parseDouble(information[7].substring(8));
-                        } else if (line.startsWith("rating: ")) {
-                            double serverRating = Double.parseDouble(information[8].substring(9));
-                        }
-                    }
-                }
-
-                //Comparator to compare against ratings and pricing across products given
-                
-            br.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static int readBudget(BufferedReader br) throws IOException {
-        InputStream is = clientSocket.getInputStream();
-        OutputStream os = clientSocket.getOutputStream();
-        InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-        OutputStreamWriter osw = new OutputStreamWriter(clientSocket.getOutputStream());
-        BufferedWriter bw = new BufferedWriter(osw);
-        BufferedReader br2 = new BufferedReader(isr);
-
-        String line = br.readLine();
-        if (line.startsWith("budget: ")) {
-            line = line.substring(9);
-            int serverBudget = Integer.parseInt(line);
-
-            return serverBudget;
-            } 
-    }
-
-    private static List<Product> selectProducts(List<Product> products, double budget) {
-        products.sort(Comparator.comparing(Product::getRating).reversed().thenComparing(Product::getPrice).reversed());
-        List<Product> selectedProducts = new ArrayList<>();
-        double remainingBudget = budget;
-
-        for (Product product : products) {
-            if (product.getPrice() <= remainingBudget) {
-                selectedProducts.add(product);
-                remainingBudget -= product.getPrice();
+    public static void main(String[] args) throws IOException {
+        //
+        if (args.length == 0) { //no parameters provided, uses default
+            server = "localhost";
+            port = 3000;
+        } else if (args.length == 1) { //
+            try {
+                port = Integer.parseInt(args[0]); //safer to wrap it in a try catch block
+            } catch (Exception e) {
+                System.out.println("Not valid port number");
+                System.exit(1); //1 means program exits abnormally, 0 - proper exit
             }
-        }
-        double spent = budget - remainingBudget;
-
-        return selectedProducts;
-    }
-
-    private static void sendSelectedProducts(BufferedWriter bw, List<Product> selectedProducts) throws IOException {
-        for (Product product : selectedProducts) {
-            bw.write("request_id: " + requestID + "\n");
-            bw.write("name: Anjali\n");
-            bw.write("email: anjali@gmail.com" + "\n");
-            bw.write("items: " + product.getTitle() + "\n");
-            bw.write("spent: " + spent + "\n");
-            bw.write("remaining" + remainingBudget + "\n");
-            bw.write("client_end\n");
+            
+        } else if (args.length == 2) { //given in assignment that is server, port
+            server = args[0];
+            port = Integer.parseInt(args[1]); 
+        } else {
+            System.out.println("invalid command, try again"); 
         }
 
-        bw.close();
+        System.out.println("Connecting to server...");
+        Socket s = new Socket(server, port);
+        System.out.println("Connected to server");
 
+        
+        
+        InputStream is = s.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line2 = "";
+
+        while ((line2 = br.readLine()) != null) { //inital method will read 1st line over and over again
+            System.out.println(line2);
+        }
+
+        String line;
+        line = br.readLine();
+        int count = 0;
+        int item_count = Integer.MAX_VALUE;
+
+        String key;
+        String value;
+
+        while (line != null) {
+            if (line.length() <= 0) { //to ignore null lines
+                continue;
+            }
+            if (line.contains(":")) {
+                String [] lines = line.trim().split(":");
+                key = lines[0].trim();
+                value = lines[1].trim();
+            } else {
+                key = line;
+                value = "";
+            }
+
+            String requestID = "";
+            String itemCount = "";
+            double budgetDouble = 0;
+            int prodId = 0;
+            String title = "";
+            double price = 0;
+            double rating = 0;
+
+            switch (key) {
+                case ProductResponse.REQUEST_ID:
+                    productResponse.setRequestId(value);
+                    break;
+                case ProductResponse.ITEM_COUNT:
+                    itemCount = Integer.parseInt(value);
+                    productResponse.setItemCount(itemCount);
+                    break;
+                case ProductResponse.BUDGET:
+                    productResponse.setBudget(Double.parseDouble(value));
+                    break;
+                case ProductResponse.PROD_START:
+                    product = new Product();
+                    break;
+                case ProductResponse.PROD_ID:
+                    product.setId(value);
+                    break;
+                case ProductResponse.TITLE:
+                    product.setTitle(value);
+                    break;
+                case ProductResponse.PRICE:
+                    product.setPrice(Double.parseDouble(value));
+                    break;
+                case ProductResponse.RATING:
+                    product.setRating(Double.parseDouble(value));
+                    break;
+                case ProductResponse.PROD_END:
+                    products.add(product);
+                    count++;
+                    break;
+            }
+            System.out.println("Completed parsing information from server.");
+
+            if (count == item_count) {
+                break;
+            }
+
+        }
+        productResponse.setProducts(
+                products.stream()
+                .sorted((a, b) -> a.getRating().intValue() - b.getRating().intValue())
+                .toList()
+            );
+
+         List<Product> products2 = productResponse.getProducts();
+         double budget = productResponse.getBudget();
+         double spent = 0.0d;
+         List<String> requestItems = new LinkedList<>();
+         for (Product product2 : products2) {
+                requestItems.add(product2.getId());
+                spent += product2.getPrice();
+                if ((budget -= product2.getPrice()) <= 0) {
+                    break;
+                }
+         }
+
+        ProductRequest productRequest = new ProductRequest();
+         productRequest.setRequestId(productResponse.getRequestId());
+         productRequest.setName("anjali");
+         productRequest.setEmail("anjali006@e.ntu.edu.sg");
+         productRequest.setItems(requestItems
+            .stream()
+            .collect(Collectors.joining(","))
+        );
+        productRequest.setSpent(spent);
+        productRequest.setRemaining(budget - spent);
+
+        OutputStream os = s.getOutputStream();
+            OutputStreamWriter osr = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osr);
+
+            bw.write("%s: %s\n".formatted(ProductRequest.REQUEST_ID, productRequest.getRequestId()));
+            bw.write("%s: %s\n".formatted(ProductRequest.NAME, productRequest.getName()));
+            bw.write("%s: %s\n".formatted(ProductRequest.EMAIL, productRequest.getEmail()));
+            bw.write("%s: %s\n".formatted(ProductRequest.ITEMS, productRequest.getItems()));
+            bw.write("%s: %s\n".formatted(ProductRequest.SPENT, productRequest.getSpent()));
+            bw.write("%s: %s\n".formatted(ProductRequest.REMAINING, productRequest.getRemaining()));
+            bw.write("%s\n".formatted(ProductRequest.CLIENT_END));
+            bw.flush();
+        
+
+        InputStream is2 = s.getInputStream();
+            InputStreamReader isr2 = new InputStreamReader(is);
+            BufferedReader br2 = new BufferedReader(isr);
+            String line3;
+            while ((line3 = br2.readLine()) != null) {
+                System.out.println(line2);
+            }
+
+            br.close();
+            isr.close();
+            is.close();
+            bw.close();
+            osr.close();
+            os.close();
+            br2.close();
+            isr2.close();
+            is2.close();
+            s.close();
     }
-
+}
--- sdf-assessment-template/task02/src/ShoppingBudget/Product.java
+++ sdf-assessment-template/task02/src/ShoppingBudget/Product.java
@@ -1,25 +1,61 @@
 package ShoppingBudget;
 
-public class Product {
-    private String title;
-    private int price;
-    private int rating;
+import java.util.List;
 
-    public Product(String title, int price, int rating) {
-        this.title= title;
+public class Product{
+
+    private int id;
+    private String title;
+    private double price;
+    private double rating;
+    
+
+    public Product(int id, String title, double price, double rating) {
+        this.id = id;
+        this.title = title;
         this.price = price;
         this.rating = rating;
     }
 
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public String getTitle() {
         return title;
     }
 
-    public int getPrice() {
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
         return price;
     }
 
-    public int getRating() {
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getRating() {
         return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public List<Product> getProducts() {
+        return null;
+    }
+
+    public double getBudget() {
+        return 0;
     }
 }
